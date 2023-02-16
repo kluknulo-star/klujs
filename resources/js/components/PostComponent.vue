191970bdd0251c925d739dd3a7530bf18dc8dfdc
kirill-kilyakov <kluknulo@mail.ru>
2023-02-16 10:15:56 +0300

    Lesson 13 v-for v-if
    Из документации к vue 3:
    Обратите внимание, не рекомендуется использовать вместе v-if и v-for.
    Когда они указаны вместе на одном узле, у v-if будет больший приоритет, чем у v-for. И поэтому в условии v-if не будет доступа к переменным из области видимости v-for
    
    Необходимо разнести эти v-for и v-if по разным узлам, например так:
    
    <template v-for="person in persons">
    <tr v-if="person.age > 20">
    ...
    </tr>
    </template>

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,10 +1,29 @@
 <template>
     <div>
         <SinglePostComponent></SinglePostComponent>
-        <div>Name: {{ name }}</div>
-        <div>Job: {{ vasyaJob }}</div>
-        <button @click="sayHello"> Hello </button>
-        <button @click="sayHello"> Hi </button>
+
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Job</th>
+            </tr>
+            </thead>
+            <tbody>
+            <template v-for="person in personsAgeLess20">
+                <tr>
+                    <th scope="row">{{ person.id }}</th>
+                    <td>{{ person.name }}</td>
+                    <td>{{ person.age }}</td>
+                    <td>{{ person.job }}</td>
+                </tr>
+            </template>
+            </tbody>
+        </table>
+
+
     </div>
 </template>
 
@@ -16,24 +35,49 @@ export default {
 
     data() {
         return {
-            name: 'Вася',
-            age: 20,
-            married: false,
-
+            persons: [
+                {
+                    id: 1,
+                    name: 'Vasya',
+                    age: 20,
+                    job: 'coach',
+                }, {
+                    id: 2,
+                    name: 'Petya',
+                    age: 21,
+                    job: 'coder',
+                }, {
+                    id: 3,
+                    name: 'Elena',
+                    age: 18,
+                    job: 'seller',
+                }, {
+                    id: 4,
+                    name: 'Ksu',
+                    age: 25,
+                    job: 'manik',
+                }, {
+                    id: 5,
+                    name: 'Olya',
+                    age: 24,
+                    job: 'traveller',
+                },
+            ]
         }
     },
 
-    methods: {
-        sayHello() {
-            console.log('Hello world')
-        },
-
-
-    },
+    methods: {},
 
     computed: {
-        vasyaJob() {
-            return this.name + ' работает в булочной'
+        personsAgeOlder20() {
+            return this.persons.filter(function (person){
+                return person.age > 20
+            })
+        },
+        personsAgeLess20() {
+            return this.persons.filter(function (person){
+                return person.age < 20
+            })
         },
     },
 
